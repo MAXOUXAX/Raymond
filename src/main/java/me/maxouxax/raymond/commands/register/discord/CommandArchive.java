b7a4fe8fb0d86dbe84b084cda2de4a97a91062fa
maxouxax <24844231+MAXOUXAX@users.noreply.github.com>
2022-02-09 15:16:32 +0100

    Edited messages to french

--- src/main/java/me/maxouxax/raymond/commands/register/discord/CommandArchive.java
+++ src/main/java/me/maxouxax/raymond/commands/register/discord/CommandArchive.java
@@ -23,9 +23,9 @@ public class CommandArchive {
         Guild guild = textChannel.getGuild();
         ServerConfig serverConfig = raymond.getServerConfigsManager().getServerConfig(guild.getId());
         if (serverConfig.isArchived()) {
-            slashCommandInteractionEvent.getHook().sendMessage("This server is already archived.").queue();
+            slashCommandInteractionEvent.getHook().sendMessage("Ce serveur est déjà archivé, utilisez /unarchive pour le désarchiver").queue();
         } else {
-            slashCommandInteractionEvent.getHook().sendMessage("This server is now archived.").queue();
+            slashCommandInteractionEvent.getHook().sendMessage("Serveur archivé !").queue();
             List<GuildChannel> channelsList = guild.getChannels();
             Role atEveryone = guild.getRolesByName("@everyone", true).get(0);
             channelsList.forEach(channel -> {
@@ -45,24 +45,21 @@ public class CommandArchive {
     public void unarchive(User user, TextChannel textChannel, SlashCommandInteractionEvent slashCommandInteractionEvent, String[] args) {
         Guild guild = textChannel.getGuild();
         ServerConfig serverConfig = raymond.getServerConfigsManager().getServerConfig(guild.getId());
-        if(!serverConfig.isArchived()){
-            slashCommandInteractionEvent.getHook().sendMessage("This server is not archived.").queue();
-        }else {
-            slashCommandInteractionEvent.getHook().sendMessage("This server is no longer archived.").queue();
+        if (!serverConfig.isArchived()) {
+            slashCommandInteractionEvent.getHook().sendMessage("Ce serveur n'est pas archivé, utilisez /archive pour l'archiver").queue();
+        } else {
+            slashCommandInteractionEvent.getHook().sendMessage("Serveur restauré !").queue();
             serverConfig.setArchived(false, true);
-        }
-        /*List<GuildChannel> channelsList = guild.getChannels();
-        channelsList.forEach(channel -> {
-            List<PermissionOverride> permissions = channelsHashMap.get(channel);
-            permissions.forEach(permissionOverride -> {
-                channel.getPermissionContainer().putPermissionOverride(permissionOverride.getPermissionHolder())
-                        .setAllow(permissionOverride.getAllowedRaw())
-                        .setDeny(permissionOverride.getDeniedRaw())
-                        .queue();
+            List<GuildChannel> channelsList = guild.getChannels();
+            channelsList.forEach(channel -> {
+                List<PermissionOverride> permissions = serverConfig.getPermissionBeforeArchive().get(channel);
+                permissions.forEach(permissionOverride -> {
+                    channel.getPermissionContainer().putPermissionOverride(permissionOverride.getPermissionHolder())
+                            .setAllow(permissionOverride.getAllowedRaw())
+                            .setDeny(permissionOverride.getDeniedRaw())
+                            .queue();
+                });
             });
-        });
-        channelsHashMap.clear();
-         */
+        }
     }
-
 }
