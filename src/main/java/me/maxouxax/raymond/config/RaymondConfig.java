525fd8ecc3dc5f8f856dddae119ba5349a593eac
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-04-20 20:33:32 +0200

    Added configuration for univ stuff

--- src/main/java/me/maxouxax/raymond/config/RaymondConfig.java
+++ src/main/java/me/maxouxax/raymond/config/RaymondConfig.java
@@ -4,8 +4,18 @@ import me.maxouxax.supervisor.supervised.Config;
 
 public class RaymondConfig extends Config {
 
+    private UnivConfig univConfig;
+
     public RaymondConfig() {
         super();
+    }
+
+    public UnivConfig getUnivConfig() {
+        return univConfig;
+    }
+
+    public void setUnivConfig(UnivConfig univConfig) {
+        this.univConfig = univConfig;
     }
 
 }
