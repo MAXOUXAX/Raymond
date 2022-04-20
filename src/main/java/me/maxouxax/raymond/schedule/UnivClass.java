6ea43c4cdbc81034963288e66fd385118c1547ef
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-04-21 00:06:12 +0200

    Enhanced Embed

--- src/main/java/me/maxouxax/raymond/schedule/UnivClass.java
+++ src/main/java/me/maxouxax/raymond/schedule/UnivClass.java
@@ -2,14 +2,16 @@ package me.maxouxax.raymond.schedule;
 
 import me.maxouxax.raymond.Raymond;
 import me.maxouxax.supervisor.utils.EmbedCrafter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.json.JSONObject;
 
 import java.awt.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.HashMap;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.*;
 
 public class UnivClass {
 
@@ -37,17 +39,34 @@ public class UnivClass {
         this.groups = event.getJSONArray("groups").toList().stream().map(o -> (HashMap<String, String>) o).map(JSONObject::new).map(Group::new).toList();
     }
 
-    public EmbedCrafter toEmbed(){
+    public EmbedCrafter toEmbed() {
         EmbedCrafter embed = new EmbedCrafter(Raymond.getInstance());
         embed.setTitle(course.getLabel());
         embed.setColor(Color.decode(course.getColor()));
+
+        DateTimeFormatter hourFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("EEEE d MMMM").withLocale(Locale.FRANCE);
+        embed.setAuthor(startDateTime.format(dayFormatter) + " — " + startDateTime.format(hourFormatter) + "-" + endDateTime.format(hourFormatter), "https://google.com", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Icons8_flat_clock.svg/1200px-Icons8_flat_clock.svg.png");
+
+        embed.setDescription("\u200E");
+
         embed.addField("Groupe(s)", groups.stream().map(Group::getLabel).reduce((a, b) -> a + ", " + b).orElse("Aucun"), true);
         embed.addField("Salle(s)", rooms.stream().map(Room::getLabel).reduce((a, b) -> a + ", " + b).orElse("Aucune"), true);
-        embed.addField("Enseignant(s)", teachers.stream().map(Teacher::getName).reduce((a, b) -> a + ", " + b).orElse("Aucun"), true);
-        embed.addField("Durée", duration / 60 + "h", true);
-        embed.addField("Date", startDateTime.toLocalDate().toString(), true);
-        embed.setDescription(course.getUrl() != null ? "[Voir le cours](" + course.getUrl() + ")" : "");
+        embed.addField("Enseignant(s)", teachers.stream().map(Teacher::getName).reduce((a, b) -> a + ", " + b).orElse("Aucun"), false);
+
         return embed;
+    }
+
+    public Collection<? extends ActionRow> toActionRow() {
+        List<ActionRow> rows = new ArrayList<>();
+        if (course.getUrl() != null) {
+            Button button = Button.link(course.getUrl(), "Voir le cours");
+            rows.add(ActionRow.of(button));
+        }
+        if (teachers.size() > 0) {
+            rows.add(ActionRow.of(teachers.stream().map(teacher -> Button.link("https://api.maxouxax.me/redirect?l=mailto:" + teacher.getEmail(), "\uD83D\uDCE7 Contacter " + teacher.getName())).toList()));
+        }
+        return rows;
     }
 
     public String getId() {
@@ -89,4 +108,5 @@ public class UnivClass {
     public List<Group> getGroups() {
         return groups;
     }
+
 }
