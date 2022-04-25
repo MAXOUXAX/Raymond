a5b802263dd17f21671f7e628b5ec12e6bd9df94
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-04-25 21:18:45 +0200

    Enhanced CommandSchedule arguments explanation and date logic

--- src/main/java/me/maxouxax/raymond/commands/register/discord/CommandSchedule.java
+++ src/main/java/me/maxouxax/raymond/commands/register/discord/CommandSchedule.java
@@ -14,30 +14,33 @@ import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
 import java.awt.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.*;
 
 public class CommandSchedule implements DiscordCommand {
 
     @Override
-    @Option(name = "jour", description = "Jour de la semaine", isRequired = true, type = OptionType.STRING)
+    @Option(name = "date", description = "Date (ex. 12/01/2022) dont vous souhaitez consulter l'emploi du temps", isRequired = true, type = OptionType.STRING)
     public void onCommand(TextChannel textChannel, Member member, SlashCommandInteractionEvent slashCommandInteractionEvent) {
-        String jour = slashCommandInteractionEvent.getOption("jour").getAsString();
-        String[] split = jour.split("/");
+        String dateInput = slashCommandInteractionEvent.getOption("date").getAsString();
+        String[] split = dateInput.split("/");
         if (split.length != 3) {
             slashCommandInteractionEvent.reply("Format de date invalide, il doit être au format DD/MM/YYYY").setEphemeral(true).queue();
         } else {
             int day = Integer.parseInt(split[0]);
             int month = Integer.parseInt(split[1]);
             int year = Integer.parseInt(split[2]);
-            if (day < 1 || day > 31 || month < 1 || month > 12 || year < 2020 || year > 2024) {
-                slashCommandInteractionEvent.reply("Date invalide, elle doit être au format DD/MM/YYYY").queue();
+            if(split[2].length() == 2) year = Integer.parseInt("20" + split[2]);
+            if (day < 1 || day > 31 || month < 1 || month > 12 || year < 2020 || year > 2099) {
+                slashCommandInteractionEvent.reply("Format de date invalide, il doit être au format DD/MM/YYYY").setEphemeral(true).queue();
             } else {
                 slashCommandInteractionEvent.deferReply().queue();
                 GregorianCalendar gregorianCalendar = new GregorianCalendar(year, month - 1, day);
-                Date date = gregorianCalendar.getTime();
+                ZonedDateTime date = gregorianCalendar.toZonedDateTime();
                 gregorianCalendar.add(Calendar.HOUR, 23);
-                Date midnight = gregorianCalendar.getTime();
+                ZonedDateTime midnight = gregorianCalendar.toZonedDateTime();
                 ArrayList<UnivClass> univSchedule = UnivHelper.getUnivSchedule(date, midnight);
                 if (univSchedule.size() == 0) {
                     EmbedCrafter embedCrafter = new EmbedCrafter(Raymond.getInstance());
@@ -54,7 +57,8 @@ public class CommandSchedule implements DiscordCommand {
                             .toList();
 
                     RestAction.allOf(messageActions).queue(messages -> {
-                        slashCommandInteractionEvent.getHook().deleteOriginal().queue();
+                        DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("EEEE d MMMM").withLocale(Locale.FRANCE);
+                        slashCommandInteractionEvent.getHook().editOriginal("Voici les cours de la journée du " + dayFormatter.format(date) + ":tada: ").queue();
                     });
                 }
 
