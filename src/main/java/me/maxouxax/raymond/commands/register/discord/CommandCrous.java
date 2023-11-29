c044d88886fe5b4ab3abaf06e4020fb0a1d143d4
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-11-29 23:19:07 +0100

    feat(crous-menu): improved crous command to set parameters

--- src/main/java/me/maxouxax/raymond/commands/register/discord/CommandCrous.java
+++ src/main/java/me/maxouxax/raymond/commands/register/discord/CommandCrous.java
@@ -4,13 +4,21 @@ import me.maxouxax.multi4j.MultiHelper;
 import me.maxouxax.multi4j.crous.CrousMenu;
 import me.maxouxax.multi4j.crous.CrousRestaurant;
 import me.maxouxax.raymond.Raymond;
+import me.maxouxax.raymond.config.RaymondServerConfig;
 import me.maxouxax.raymond.config.UnivServerConfig;
 import me.maxouxax.raymond.utils.CrousUtils;
+import me.maxouxax.supervisor.interactions.annotations.Option;
+import me.maxouxax.supervisor.interactions.annotations.Subcommand;
+import me.maxouxax.supervisor.interactions.annotations.SubcommandGroup;
 import me.maxouxax.supervisor.interactions.commands.DiscordCommand;
 import me.maxouxax.supervisor.utils.EmbedCrafter;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -58,6 +66,47 @@ public class CommandCrous implements DiscordCommand {
         } catch (IOException | URISyntaxException | InterruptedException | ParseException e) {
             raymond.getLogger().error("Failed to get CROUS menu for guild " + member.getGuild().getId() + " (" + member.getGuild().getName() + ")", e);
         }
+    }
+
+    @SubcommandGroup(name = "config", description = "Configurez les paramètres des restaurants CROUS")
+    @Subcommand(name = "set-channel", description = "Définissez le salon dans lequel envoyer le menu du CROUS")
+    @Option(name = "channel", description = "Le salon dans lequel envoyer le menu du CROUS", type = OptionType.CHANNEL, required = true)
+    public void onCROUSSetChannel(MessageChannelUnion messageChannelUnion, Member member, SlashCommandInteractionEvent slashCommandInteractionEvent) {
+        GuildChannelUnion channel = slashCommandInteractionEvent.getOption("channel").getAsChannel();
+        if (channel.getType() != ChannelType.TEXT) {
+            slashCommandInteractionEvent.reply("Le salon doit être un salon texte").queue();
+            return;
+        }
+
+        RaymondServerConfig serverConfig = raymond.getServerConfigsManager().getServerConfig(slashCommandInteractionEvent.getGuild().getId());
+        serverConfig.getUnivServerConfig().setDiscordCrousMenuChannelId(channel.getId());
+        serverConfig.saveConfig();
+
+        slashCommandInteractionEvent.reply("Le salon dans lequel envoyer le menu du CROUS a été défini").queue();
+    }
+
+    @SubcommandGroup(name = "config", description = "Configurez les paramètres des restaurants CROUS")
+    @Subcommand(name = "set-role", description = "Définissez le rôle à notifier lors de la publication du menu du CROUS")
+    @Option(name = "role", description = "Le rôle à notifier lors de la publication du menu du CROUS", type = OptionType.ROLE, required = true)
+    public void onCROUSSetRole(MessageChannelUnion messageChannelUnion, Member member, SlashCommandInteractionEvent slashCommandInteractionEvent) {
+        Role role = slashCommandInteractionEvent.getOption("role").getAsRole();
+        RaymondServerConfig serverConfig = raymond.getServerConfigsManager().getServerConfig(slashCommandInteractionEvent.getGuild().getId());
+        serverConfig.getUnivServerConfig().setDiscordCrousMenuRoleId(role.getId());
+        serverConfig.saveConfig();
+
+        slashCommandInteractionEvent.reply("Le rôle à notifier lors de la publication du menu du CROUS a été défini").queue();
+    }
+
+    @SubcommandGroup(name = "config", description = "Configurez les paramètres des restaurants CROUS")
+    @Subcommand(name = "set-restaurant-name", description = "Définissez le nom du restaurant CROUS")
+    @Option(name = "restaurant-name", description = "Le nom du restaurant CROUS", type = OptionType.STRING, required = true)
+    public void onCROUSSetRestaurant(MessageChannelUnion messageChannelUnion, Member member, SlashCommandInteractionEvent slashCommandInteractionEvent) {
+        String restaurantName = slashCommandInteractionEvent.getOption("restaurant-name").getAsString();
+        RaymondServerConfig serverConfig = raymond.getServerConfigsManager().getServerConfig(slashCommandInteractionEvent.getGuild().getId());
+        serverConfig.getUnivServerConfig().setCrousRestaurantName(restaurantName);
+        serverConfig.saveConfig();
+
+        slashCommandInteractionEvent.reply("Le nom du restaurant CROUS a été défini").queue();
     }
 
     @Override
