14c495d149b4b1683adeeb5ad774670c0b7f3609
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-11-29 22:26:20 +0100

    chore(univ-config): added required fields inside the UnivServerConfig class

--- src/main/java/me/maxouxax/raymond/config/UnivServerConfig.java
+++ src/main/java/me/maxouxax/raymond/config/UnivServerConfig.java
@@ -15,6 +15,15 @@ public class UnivServerConfig {
     @BsonProperty("discord_forum_role_id")
     private String discordForumRoleId;
 
+    @BsonProperty("crous_restaurant_name")
+    private String crousRestaurantName;
+
+    @BsonProperty("discord_crous_menu_channel_id")
+    private String discordCrousMenuChannelId;
+
+    @BsonProperty("discord_crous_menu_role_id")
+    private String discordCrousMenuRoleId;
+
     public UnivServerConfig() {
         this.univSchedule = new UnivSchedule();
     }
@@ -22,10 +31,17 @@ public class UnivServerConfig {
     @BsonCreator
     public UnivServerConfig(@BsonProperty("univ_schedule") final UnivSchedule univSchedule,
                             @BsonProperty("discord_forum_channel_id") final String discordForumChannelId,
-                            @BsonProperty("discord_forum_role_id") final String discordForumRoleId) {
+                            @BsonProperty("discord_forum_role_id") final String discordForumRoleId,
+                            @BsonProperty("crous_restaurant_name") final String crousRestaurantName,
+                            @BsonProperty("discord_crous_menu_channel_id") final String discordCrousMenuChannelId,
+                            @BsonProperty("discord_crous_menu_role_id") final String discordCrousMenuRoleId
+    ) {
         this.univSchedule = univSchedule;
         this.discordForumChannelId = discordForumChannelId;
         this.discordForumRoleId = discordForumRoleId;
+        this.crousRestaurantName = crousRestaurantName;
+        this.discordCrousMenuChannelId = discordCrousMenuChannelId;
+        this.discordCrousMenuRoleId = discordCrousMenuRoleId;
     }
 
     public UnivSchedule getUnivSchedule() {
@@ -40,6 +56,18 @@ public class UnivServerConfig {
         return discordForumRoleId;
     }
 
+    public String getCrousRestaurantName() {
+        return crousRestaurantName;
+    }
+
+    public String getDiscordCrousMenuChannelId() {
+        return discordCrousMenuChannelId;
+    }
+
+    public String getDiscordCrousMenuRoleId() {
+        return discordCrousMenuRoleId;
+    }
+
     public void setUnivSchedule(UnivSchedule univSchedule) {
         this.univSchedule = univSchedule;
     }
@@ -52,4 +80,15 @@ public class UnivServerConfig {
         this.discordForumRoleId = id;
     }
 
+    public void setCrousRestaurantName(String crousRestaurantName) {
+        this.crousRestaurantName = crousRestaurantName;
+    }
+
+    public void setDiscordCrousMenuChannelId(String discordCrousMenuChannelId) {
+        this.discordCrousMenuChannelId = discordCrousMenuChannelId;
+    }
+
+    public void setDiscordCrousMenuRoleId(String discordCrousMenuRoleId) {
+        this.discordCrousMenuRoleId = discordCrousMenuRoleId;
+    }
 }
