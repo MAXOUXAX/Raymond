9611de4d913de59a998dea31d30abfb73e1c3624
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-11-29 23:02:32 +0100

    refactor(crous-menu): moved some utility methods to CrousUtils

---
+++ src/main/java/me/maxouxax/raymond/utils/CrousUtils.java
@@ -0,0 +1,54 @@
+package me.maxouxax.raymond.utils;
+
+import me.maxouxax.multi4j.crous.CrousFoodCategory;
+import me.maxouxax.multi4j.crous.CrousMeal;
+import me.maxouxax.multi4j.crous.CrousMenu;
+import me.maxouxax.multi4j.crous.CrousRestaurant;
+import me.maxouxax.raymond.Raymond;
+import me.maxouxax.supervisor.utils.EmbedCrafter;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Optional;
+
+public class CrousUtils {
+
+    private static final Raymond raymond = Raymond.getInstance();
+
+    public static EmbedCrafter buildEmbedFromCrous(CrousRestaurant restaurant, CrousMenu menu){
+        EmbedCrafter embedCrafter = new EmbedCrafter(raymond);
+        embedCrafter.setTitle("Menu du " + menu.getDate().toString() + " | " + restaurant.getName());
+        embedCrafter.setThumbnailUrl(restaurant.getThumbnailUrl());
+
+        StringBuilder description = new StringBuilder();
+        for (CrousMeal meal : menu.getMeals()) {
+            for (CrousFoodCategory foodCategory : meal.getFoodCategories()) {
+                description.append("## Menu du ").append(meal.getName()).append("\n");
+                description.append("### ").append(foodCategory.getName()).append("\n");
+                for (String dish : foodCategory.getDishes()) {
+                    // Skip empty dishes
+                    if(dish.trim().equalsIgnoreCase("")) continue;
+
+                    description.append("- ").append(dish).append("\n");
+                }
+            }
+        }
+        embedCrafter.setDescription(description.toString());
+        return embedCrafter;
+    }
+
+    public static Optional<CrousMenu> findTodaysMenu(CrousRestaurant restaurant) {
+        return restaurant.getMenus().stream().filter(crousMenu -> {
+            Date today = new Date();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(today);
+
+            Date menuDate = crousMenu.getDate();
+            Calendar menuCalendar = Calendar.getInstance();
+            menuCalendar.setTime(menuDate);
+
+            return calendar.get(Calendar.DAY_OF_YEAR) == menuCalendar.get(Calendar.DAY_OF_YEAR);
+        }).findFirst();
+    }
+
+}
