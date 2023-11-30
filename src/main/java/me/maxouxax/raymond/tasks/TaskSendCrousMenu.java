20a68b23fc5a75de138e8d91df6592a627d9b185
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-11-30 21:47:37 +0100

    feat(crous-menu): creating a thread channel everytime the CROUS menu is posted automatically

--- src/main/java/me/maxouxax/raymond/tasks/TaskSendCrousMenu.java
+++ src/main/java/me/maxouxax/raymond/tasks/TaskSendCrousMenu.java
@@ -9,13 +9,23 @@ import me.maxouxax.supervisor.utils.EmbedCrafter;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public class TaskSendCrousMenu implements Runnable {
+
     private final Raymond raymond;
+    private final ArrayList<String> randomMessages = new ArrayList<>();
 
     public TaskSendCrousMenu(Raymond raymond) {
         this.raymond = raymond;
+        this.randomMessages.add("On en dit quoi du menu d'aujourd'hui au %s ?");
+        this.randomMessages.add("Ça m'a l'air pas mal le menu d'aujourd'hui au %s");
+        this.randomMessages.add("J'ai faim, on mange au %s ?");
+        this.randomMessages.add("Le menu du %s a l'air pas mal aujourd'hui");
+        this.randomMessages.add("Le menu du %s est disponible !");
+        this.randomMessages.add("Je vais me régaler au %s aujourd'hui");
+        this.randomMessages.add("Je vais remplir mon ventre au %s aujourd'hui");
     }
 
     @Override
@@ -40,7 +50,12 @@ public class TaskSendCrousMenu implements Runnable {
                     CrousMenu menu = todaysMenu.get();
                     EmbedCrafter embed = CrousUtils.buildEmbedFromMenu(restaurantDetails, menu);
 
-                    crousMenuChannel.sendMessage(crousRole.getAsMention()).addEmbeds(embed.build()).queue();
+                    crousMenuChannel.sendMessage(crousRole.getAsMention()).addEmbeds(embed.build()).queue(message -> {
+                        message.createThreadChannel("Discussion autour du menu").queue(threadChannel -> {
+                            String randomMessage = randomMessages.get((int) (Math.random() * randomMessages.size()));
+                            threadChannel.sendMessage(String.format(randomMessage, restaurantDetails.getName())).queue();
+                        });
+                    });
                 }
             } catch (Exception e) {
                 raymond.getLogger().error("Failed to send CROUS menu for guild " + guild.getId() + " (" + guild.getName() + ")", e);
